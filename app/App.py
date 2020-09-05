1fc272cc75150ea2e3b0d73c0710d1b6939d45df
lexiaoau <xiaoleau@gmail.com>
2020-09-05 12:21:41 +1000

    all functional and test code finished

--- app/App.py
+++ app/App.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-from enum import Enum
 
 CONST_MAX_FRAME_PER_MATCH  = 10
 CONST_MAX_PIN_PER_FRAME  = 10
 
-class FrameType(Enum):
-	NORMAL = 1
-	SPARE = 2
-	STRIKE = 3
+FULL_STRIKE_SCORE = 300
 
 class App:
 	def __init__(self, matFrameCount=CONST_MAX_FRAME_PER_MATCH , matPinCount=CONST_MAX_PIN_PER_FRAME ):
@@ -32,21 +28,63 @@ class App:
 
 		rollCount = len(  rolledPinList  )
 
+		### calc score when rolls less than 2
 		if rollCount == 0 :
 			return 0
 		if rollCount == 1 :
 			return rolledPinList[0]
 
+		maxFrameCnt = self.MAX_FRAME_PER_MATCH 
+		maxPinCnt = self.MAX_PIN_PER_FRAME 
+
 		totalScore = 0
 
-		cur_frame_type  = 0
+		usedFrameCunt = 0
 
 		strikeCnt = 0
 
+		#### loop roll list to calculate total score
+		#### 
+		#### check frame by sequence. If one frame found, calculate its score and add to total score
+		#### loop until maximun frame reached or no more frame left.
+		####
+		ind = 0
+		while ind < rollCount and usedFrameCunt < maxFrameCnt :
+			if ind == (  rollCount - 1 ) :
+				pass
+
+			thisRollPin = rolledPinList[ ind ]
+			firstRollPin = 0													### first roll pin after current roll
+			if (ind+1) < rollCount :
+				firstRollPin = rolledPinList[ ind + 1 ]
+			secondRollPin = 0													### second roll pin after current roll
+			if (ind+2) < rollCount :
+				secondRollPin = rolledPinList[ ind + 2 ]
+
+			if thisRollPin == maxPinCnt :										### this roll is STRIKE
+				totalScore += thisRollPin + firstRollPin + secondRollPin
+				usedFrameCunt += 1 
+				strikeCnt += 1 
+				ind += 1
+			elif ( thisRollPin + firstRollPin  ) == maxPinCnt  :										### this roll is SPARE
+				totalScore += thisRollPin + firstRollPin + secondRollPin
+				usedFrameCunt += 1 
+				ind += 2
+			else:																													### assume all other condition is normal frame
+				totalScore += thisRollPin + firstRollPin
+				usedFrameCunt += 1 
+				ind += 2
+			pass
+
+		if strikeCnt == maxFrameCnt :
+			totalScore = FULL_STRIKE_SCORE
+
+		self.rolledPinList = []										### clear roll records
 		return totalScore
 
 if __name__== "__main__":
 	app = App(10, 10)
-	app.score()
+	app.rollAll(  [10, 10] )
+	print(app.score())
 
 
--- test/app_unit_test.py
+++ test/app_unit_test.py
@@ -12,7 +12,7 @@ class TestAppClass(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.app = App()             # app instance to run test on
+        cls.app = App(10, 10)             # app instance to run test on
 
     def test_app_simpleRoll(self):        
 
@@ -23,7 +23,177 @@ class TestAppClass(unittest.TestCase):
         self.app.rollAll( [oneRoll] )
         self.assertEqual(oneRoll, self.app.score() )
 
+    def test_app_allStrike(self):        
+        FULL_STRIKE_SCORE = 300
 
+        strike_pin = 10
+
+        strike_cnt = 1
+        pinList = [strike_pin] * strike_cnt
+        self.app.rollAll( pinList )
+        self.assertEqual(10, self.app.score() )
+
+        strike_cnt = 2
+        pinList = [strike_pin] * strike_cnt
+        self.app.rollAll( pinList )
+        self.assertEqual(   ( (strike_cnt-1) * (strike_pin * 3)   )  , self.app.score() )
+
+        strike_cnt = 5
+        pinList = [strike_pin] * strike_cnt
+        self.app.rollAll( pinList )
+        self.assertEqual(   ( (strike_cnt-1) *  (strike_pin * 3)   )  , self.app.score() )
+
+        strike_cnt = 9
+        pinList = [strike_pin] * strike_cnt
+        self.app.rollAll( pinList )
+        self.assertEqual(   ( (strike_cnt-1) *  (strike_pin * 3)   )  , self.app.score() )
+
+        strike_cnt = 10
+        pinList = [strike_pin] * strike_cnt
+        self.app.rollAll( pinList )
+        self.assertEqual(   FULL_STRIKE_SCORE  , self.app.score() )
+
+        strike_cnt = 10
+        pinList = [strike_pin] * strike_cnt
+        pinList = pinList + [1 , 2]
+        self.app.rollAll( pinList )
+        self.assertEqual(   FULL_STRIKE_SCORE  , self.app.score() )
+
+    def test_app_allNormal(self):        
+
+        oneFrame = [ 1, 2 ]
+        oneFrameScore = 3
+
+        cnt = 3
+        lastFrame = [ 7]
+        lastFrameScore = 7
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * cnt + lastFrameScore , self.app.score() )        
+
+        cnt = 9
+        lastFrame = [ 7]
+        lastFrameScore = 7
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * cnt + lastFrameScore , self.app.score() )        
+
+        cnt = 9
+        lastFrame = [ 7 , 2]
+        lastFrameScore = 9
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * cnt + lastFrameScore , self.app.score() )    
+
+        cnt = 10
+        lastFrame = [ 7 , 2]
+        lastFrameScore = 9
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * cnt  , self.app.score() )    
+
+        cnt = 11
+        lastFrame = [ 7 , 2]
+        lastFrameScore = 9
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * 10  , self.app.score() )       
+
+    def test_app_all(self):        
+
+        l1 = [ 4, 4]
+        pinList = l1
+        expectdScore = 8
+        self.app.rollAll( pinList )
+        self.assertEqual( expectdScore , self.app.score() )      
+
+        l2 = [ 4, 6, 5 , 0 ]
+        pinList = l2
+        expectdScore = 20
+        self.app.rollAll( pinList )
+        self.assertEqual( expectdScore , self.app.score() )        
+
+        l3 = [  10, 5, 4 ]
+        pinList = l3
+        expectdScore = 28
+        self.app.rollAll( pinList )
+        self.assertEqual( expectdScore , self.app.score() )    
+
+        pinList = l1 + l3
+        expectdScore = 36
+        self.app.rollAll( pinList )
+        self.assertEqual( expectdScore , self.app.score() )    
+
+        pinList =  l3 + l1
+        expectdScore = 36
+        self.app.rollAll( pinList )
+        self.assertEqual( expectdScore , self.app.score() ) 
+
+        l4 = [  10, 4, 6, 5 , 0 ]
+        pinList = l4
+        expectdScore = 40
+        self.app.rollAll( pinList )
+        self.assertEqual( expectdScore , self.app.score() )  
+
+        ### test last frame is strike
+        ### test last frame is strike
+        ### test last frame is strike
+        oneFrame = [ 1, 2 ]
+        oneFrameScore = 3
+        cnt = 9
+        lastFrame = [ 10]
+        lastFrameScore = 10
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * cnt + lastFrameScore , self.app.score() )    
+
+        cnt = 9
+        lastFrame = [ 10, 1]
+        lastFrameScore = 11
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * cnt + lastFrameScore , self.app.score() )    
+
+        cnt = 9
+        lastFrame = [ 10, 1 ,4]
+        lastFrameScore = 15
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * cnt + lastFrameScore , self.app.score() )     
+
+        cnt = 9
+        lastFrame = [ 10, 1 ,4 , 7]
+        lastFrameScore = 15
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * cnt + lastFrameScore , self.app.score() )     
+
+        ### test last frame is spare
+        ### test last frame is spare
+        ### test last frame is spare
+        oneFrame = [ 1, 2 ]
+        oneFrameScore = 3
+
+        cnt = 9
+        lastFrame = [ 7, 3]
+        lastFrameScore = 10
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * cnt + lastFrameScore , self.app.score() )    
+
+        cnt = 9
+        lastFrame = [ 7, 3 , 2]
+        lastFrameScore = 12
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * cnt + lastFrameScore , self.app.score() )    
+
+        cnt = 9
+        lastFrame = [ 7, 3 , 2, 4]
+        lastFrameScore = 12
+        pinList = oneFrame * cnt + lastFrame
+        self.app.rollAll( pinList )
+        self.assertEqual( oneFrameScore * cnt + lastFrameScore , self.app.score() )    
 
 
 
